5abd19532c589d5c32d7e1d8223a424cde6f27b1
Immaculate <geoffreyimmaculate@gmail.com>
2025-09-16 11:18:41 +0100

    The checker needed to see the both rank and row_number so I added it

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,8 +8,16 @@ SELECT property_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY property_id;
 
+-- Rank properties using ROW_NUMBER (always unique ranks)
 SELECT property_id,
        COUNT(*) AS total_bookings,
        ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
+FROM bookings
+GROUP BY property_id;
+
+-- Rank properties using RANK (ties allowed)
+SELECT property_id,
+       COUNT(*) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM bookings
 GROUP BY property_id;
