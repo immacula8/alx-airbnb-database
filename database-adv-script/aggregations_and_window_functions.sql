7a12e878be2af61aa60d9c1539c977c3b975c872
Immaculate <geoffreyimmaculate@gmail.com>
2025-09-16 11:15:54 +0100

    Changed Rank to Row_number

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,6 +10,6 @@ GROUP BY property_id;
 
 SELECT property_id,
        COUNT(*) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
 FROM bookings
 GROUP BY property_id;
